5c0fe077d9f4a4f56bdc78a211e5260907a91560
vict-shevchenko <vict.shevchenko@gmail.com>
2015-11-25 01:26:06 +0200

    some test js

--- welcome.js
+++ welcome.js
@@ -1,1 +1,20 @@
-welcome.js
+(function() {
+  'use strict'
+
+  var extraSearchParamsBtn = document.getElementsByClassName('welcome-page__extra-search-btn').item(0),
+      extraSearchParamsContainer = document.getElementsByClassName('welcome-page__extra-search-container').item(0);
+
+  function addEventListners() {
+    extraSearchParamsBtn.addEventListener('click', showExtraSearchParamsContainer)
+  };
+
+  function showExtraSearchParamsContainer() {
+
+
+    extraSearchParamsContainer.classList.toggle("welcome-page__extra-search-container_visible");
+  }
+
+
+  addEventListners();
+
+})();
