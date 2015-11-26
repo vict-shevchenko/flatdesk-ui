f620aae17a3d6bd3199839819b41e27a99d2b808
vict-shevchenko <vict.shevchenko@gmail.com>
2015-11-27 00:19:34 +0200

    working on slideup menu

--- welcome.js
+++ welcome.js
@@ -2,7 +2,7 @@
   'use strict'
 
   var extraSearchParamsBtn = document.getElementsByClassName('welcome-page__extra-search-btn').item(0),
-      extraSearchParamsContainer = document.getElementsByClassName('welcome-page__extra-search-container').item(0);
+      fastSearchContainer = document.getElementsByClassName('welcome-page__fast-search').item(0);
 
   function addEventListners() {
     extraSearchParamsBtn.addEventListener('click', showExtraSearchParamsContainer)
@@ -11,7 +11,7 @@
   function showExtraSearchParamsContainer() {
 
 
-    extraSearchParamsContainer.classList.toggle("welcome-page__extra-search-container_visible");
+    fastSearchContainer.classList.toggle('welcome-page__fast-search_show-extra');
   }
 
 
