4391e9317f3014ad61f95f91a971c18eb1b1f408
viktor.shevchenko <viktor@geeny.io>
2017-08-08 12:51:12 +0300

    2nd change br2

--- index.tsx
+++ index.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import * as ReactDom from 'react-dom'
 import greeter from './greeter'
 // changes in br 2 - 1st
-
+// changes in br 2 - 2nd
 const user = { firstName: "Jane", lastName: "User" };
 
 ReactDom.render(
